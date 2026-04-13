995e9b45f0e3766aa1e97208ef05d16b71082f6a
Goggy <bigggrinz@gmail.com>
2026-04-13 16:29:38 +0500

    правка

--- src/main/java/ru/yandex/practicum/filmorate/exception/UserPresenceInListException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/UserPresenceInListException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class UserPresenceInListException extends RuntimeException{
+public class UserPresenceInListException extends RuntimeException {
     public UserPresenceInListException(String message) {
         super(message);
     }
