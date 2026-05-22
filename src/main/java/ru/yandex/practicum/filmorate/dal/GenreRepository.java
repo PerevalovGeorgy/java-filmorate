4475b1de95a7f080bd3a4ec283d6aa07798c9a54
dudka-54 <kovboimolodoi@yandex.ru>
2026-05-23 00:36:35 +0400

    исправление тестов

--- src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
@@ -17,12 +17,12 @@ public class GenreRepository {
     private final GenreRowMapper genreRowMapper;
 
     public Collection<Genre> findAll() {
-        String sql = "SELECT id FROM Genre ORDER BY id ASC";
+        String sql = "SELECT id, name FROM genres ORDER BY id ASC ";
         return jdbc.query(sql, genreRowMapper);
     }
 
     public Optional<Genre> findById(Integer id) {
-        String sql = "SELECT id FROM Genre WHERE id = ?";
+        String sql = "SELECT id, name FROM genres WHERE id = ?";
         List<Genre> genres = jdbc.query(sql, genreRowMapper, id);
         return genres.stream().findFirst();
     }
