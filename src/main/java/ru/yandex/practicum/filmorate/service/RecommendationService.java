9f751399c474ae10cb8f7b1400580a70cbe0604d
dudka-54 <kovboimolodoi@yandex.ru>
2026-05-25 13:26:58 +0400

    исправления

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.mapper.UserMapper;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.*;
 import java.util.function.Predicate;
